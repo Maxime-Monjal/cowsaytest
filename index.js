404ac2cd0e04851fbd860978b87d1441adf3bfe9
Maxime-Monjal <maxime.monjal@gmail.com>
2020-10-01 12:48:00 +0200

    Finish the quest

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const myInfo = require('/home/wilder/Desktop/cowsaytest/information.js');
 const cowsay = require("cowsay");
 
+
 console.log(cowsay.say({
-    text : "Hello I'm Max from Nantes Campus! (Enjoy and have a good day)",
+    text : `Hello I'm ${myInfo.name} from ${myInfo.campus} Campus! (Enjoy and have a good day)`,
     e : "oO",
     T : "U ",
 }));
